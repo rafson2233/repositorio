789313cc8b1704246ef7e880d5161f7bddc33247
Beatriz <beatriz.wg@hotmail.com>
2025-05-24 14:20:17 -0300

    Feat: Criação do backend da página de login

--- back/app.js
+++ back/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const registerRoutes = require('./routes/RegisterRoutes');
+const authRoutes = require('./routes/AuthRoutes');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(registerRoutes); 
+app.use(registerRoutes);
+app.use(authRoutes); 
 
 module.exports = app;
