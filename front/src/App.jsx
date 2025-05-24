f908bb3aebf7e70482c980ab9301e0ecb8628fd9
wellington Rangel <rangelwellington18@gmail.com>
2025-05-24 14:53:58 -0300

    Feat: Criação do front da página de registro de ticket

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import  Home  from "./pages/Home/Home";
+import Home from "./pages/Home/Home";
 import Header from './components/header/PageHeader';
 import About from "./pages/About/About";
 import Register from './pages/Register/Register';
@@ -9,13 +9,23 @@ import Login from './pages/Login/Login';
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/header" element={<Header/>}/> 
-      <Route path="/" element={<Home/>}/> 
-      <Route path="/about" element ={<About/>}/>
-      <Route path="/register" element={<Register/>}/> 
-      <Route path="/login" element={<Login/>}/> 
-    </Routes>
+      <Routes>
+        <Route path="/header" element={<Header />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/ticketRegistration"
+          element={
+            <ProtectedRoute>
+              <TicketRegistration />
+            </ProtectedRoute>
+          }
+        />
+
+      </Routes>
     </>
   )
 }
